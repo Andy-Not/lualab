2f2d808d2b8f0f03717b8d0b89c88e28c33094c5
joandyalejo <joandyalejogarcia@outlook.com>
2024-11-11 15:02:00 -0500

    basic guess game created

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
-print("h3ll0 w0rld")
+print("Please enter a number range: ");
+local min = io.read("*n");
+print("you picked number range of : ",min);
+print("Now take a guess pick a number from 0 to "..min);
+local userGuess = io.read("*n");
+local number = math.random(min);
+
+if userGuess == number then 
+    print("You guessed right! The number was: "..number);
+    else
+    print("You guessed wrong! The number was: "..number);
+end
